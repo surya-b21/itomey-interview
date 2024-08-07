62fac925052a22ea8f3768ca5f0bf89e1910574a
surya b <a123@MacBook-Air-M1-TBRG.local>
2024-08-07 10:12:21 +0700

    first init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5-i; j++ {
+			fmt.Print(" ")
+		}
+
+		for j := 0; j <= i; j++ {
+			fmt.Print(1)
+			fmt.Print(" ")
+		}
+
+		fmt.Println()
+	}
+}
